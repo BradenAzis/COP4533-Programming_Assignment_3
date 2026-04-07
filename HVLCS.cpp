8719114ca50b5a579439a7f401b2ab5773ff4ded
BradenAzis <bradenkai747@gmail.com>
2026-04-06 22:06:40 -0400

    Full file draft

--- HVLCS.cpp
+++ HVLCS.cpp
@@ -6,8 +6,20 @@ using namespace std;
 
 int main() {
 
+    // num chars in alphabet
+    int k;
+    cin >> k;
+
     // map of values per character
     unordered_map<char, long long> val;
+
+    // loop through the chars and their corresponding value
+    for (int i = 0; i < k; i++) {
+        char c;
+        long long v;
+        cin >> c >> v;
+        val[c] = v;
+    }
 
     string A, B;
     cin >> A >> B;
@@ -21,7 +33,6 @@ int main() {
     // TODO: DP Algorithm
     for (int i = n - 1; i >= 0; i--) {
         for (int j = m - 1; j >= 0; j--) {
-
             if (A[i] == B[j]) {
                 DP[i][j] = val[A[i]] + DP[i+1][j+1];
             } else {
@@ -30,7 +41,33 @@ int main() {
         }
     }
 
-    // TODO: String Reconstruction?
+    // Reconstruction
+    string result = "";
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (A[i] == B[j]) {
+            // Must check if this choice achieves DP[i][j]
+            if (DP[i][j] == val[A[i]] + DP[i+1][j+1]) {
+                result.push_back(A[i]);
+                i++;
+                j++;
+            } else {
+                // Character matches but DP chooses skipping
+                if (DP[i+1][j] >= DP[i][j+1]) 
+                    i++;
+                else 
+                    j++;
+            }
+        } else {
+            // Move to better direction
+            if (DP[i+1][j] >= DP[i][j+1]) i++;
+            else j++;
+        }
+    }
+
+    // Output
+    cout << DP[0][0] << "\n";
+    cout << result << "\n";
 
 
 
